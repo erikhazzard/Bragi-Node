02eec03064bd113229f181721b1d328543634d87
Erik <enoex1@gmail.com>
2014-08-17 20:25:02 -0700

    updates example calls function

--- README.md
+++ README.md
@@ -78,6 +78,9 @@ To specify a blacklist, use `groupsDisabled`. This would log everything *except*
 
 `logger.options.groupsEnabled = true; logger.options.groupsDisabled = ['group1'];`  
 
+### Built in log types
+Currently only two built in log types exist: `error` and `warn`. These types can also be namespaced (e.g., `error:group1:subgroup1` is valid). For error messages, the background will always be red and the foreground white. For warn messages, the background is yellow and foreground is white. The text will also blink. These are reserved colors, so anywhere a red background and white text exist you can immediately know an error has been logged.
+
 ### Examples
 In the `examples` folder, there are various examples of calling and configuring Bragi.
 
--- examples/example-calls.js
+++ examples/example-calls.js
@@ -41,69 +41,92 @@ logger.log('group1', 'Top level group log');
 logger.log('group1:subgroup1', 'subgroup message');
 logger.log('group1:subgroup1:subgroup2', 'nested subgroup message');
 
-// Set log level (specifying which logs to show
-logger.options.groupsEnabled = ['group1'];
-logger.log('group1', 'Shown');
-logger.log('group2', 'Not shown');
+var baseDelay = 700;
 
-// Set log level - sub groups
-logger.options.groupsEnabled = ['group1:subgroup1'];
-logger.log('group1:subgroup1', 'Shown');
-logger.log('group1:subgroup2', 'Shown');
+setTimeout(function intialTimeout(){
+    // Set log level (specifying which logs to show
+    logger.options.groupsEnabled = ['group1'];
+    logger.log('group1', 'Shown');
+    logger.log('group2', 'Not shown');
 
-
-// --------------------------------------
-// Show multiple logs
-// --------------------------------------
-// Everything
-logger.options.groupsEnabled = true;
-
-// --------------------------------------
-// Some built in UTF8 symbols
-// --------------------------------------
-logger.log('mockRequest:sendData', logger.util.print(logger.util.symbols.arrow, 'yellow') + ' making request...');
-logger.log('mockRequest:sendData:success', logger.util.symbols.success + ' returned success', { status: 200 });
-logger.log('mockRequest:sendData:error', logger.util.symbols.error + ' returned an error!', { status: 500 });
-
-logger.log('group2', logger.util.symbols.box + ' Starting an async request');
-logger.log('group2', '\t' + logger.util.symbols.boxSuccess + ' it worked');
-logger.log('group2', '\t' + logger.util.symbols.boxError + ' or maybe it failed');
-
-// --------------------------------------
-// Printing colors
-// --------------------------------------
-// We can also use colors
-logger.log(
-    'group3:showColor', 
-    logger.util.print('I am red', 'red') + " but I am not red"
-);
-
-// or pass in symbols too - first param is just a string
-logger.log(
-    'group3:symbols', 
-    logger.util.print(logger.util.symbols.circle, 'yellow') + " yellow circle "
-);
-logger.log(
-    'group3:symbols', 
-    logger.util.print(logger.util.symbols.circleFilled, 'green') + " nom nom filled circle"
-);
+    // Set log level - sub groups
+    logger.options.groupsEnabled = ['group1:subgroup1'];
+    logger.log('group1:subgroup1', 'Shown');
+    logger.log('group1:subgroup2', 'Shown');
 
 
-// --------------------------------------
-// log a bunch of stuff!
-// --------------------------------------
-var message;
-for(var i=0; i<30; i++){
-    message = "I am a log message! ";
-    for(var j=0; j<(Math.random() * 5|0); j++){
-        message += message;
-    }
-    logger.log('group' + i, message);
-}
+    // --------------------------------------
+    // Show multiple logs
+    // --------------------------------------
+    setTimeout(function asyncExamples (){
+        // Everything
+        logger.options.groupsEnabled = true;
+
+        // --------------------------------------
+        // Some built in UTF8 symbols
+        // --------------------------------------
+        logger.log('mockRequest:sendData', logger.util.print(logger.util.symbols.arrow, 'yellow') + ' making request...');
+
+        setTimeout(function asyncExampleComplete(){ 
+            logger.log('mockRequest:sendData:success', logger.util.symbols.success + ' returned success', { status: 200 });
+            logger.log('mockRequest:sendData:error', logger.util.symbols.error + ' returned an error!', { status: 500 });
+
+            setTimeout(function asyncExample2(){ 
+
+                // Can also use box symbols
+                logger.log('group2', logger.util.symbols.box + ' Starting an async request');
+
+                setTimeout(function asyncExample2Complete(){ 
+                    logger.log('group2', '\t' + logger.util.symbols.boxSuccess + ' it worked');
+                    logger.log('group2', '\t' + logger.util.symbols.boxError + ' or maybe it failed');
+
+                    // --------------------------------------
+                    // Printing colors
+                    // --------------------------------------
+                    setTimeout(function showColorsExample () {
+                        // We can also use colors
+                        logger.log(
+                            'group3:showColor', 
+                            logger.util.print('I am red', 'red') + " but I am not red"
+                        );
+
+                        // or pass in symbols too - first param is just a string
+                        logger.log(
+                            'group3:symbols', 
+                            logger.util.print(logger.util.symbols.circle, 'yellow') + " yellow circle "
+                        );
+                        logger.log(
+                            'group3:symbols', 
+                            logger.util.print(logger.util.symbols.circleFilled, 'green') + " nom nom filled circle"
+                        );
 
 
-// --------------------------------------
-// Finally, show some reserved messages
-// --------------------------------------
-logger.log('warn', 'This is a warning!', {message: 'It may not work'});
-logger.log('error', 'This is an error!', {message: 'It did not work'});
+                        // --------------------------------------
+                        // log a bunch of stuff!
+                        // --------------------------------------
+                        setTimeout(function showManyLogs () {
+                            var message;
+                            for(var i=0; i<30; i++){
+                                message = "I am a log message! ";
+                                for(var j=0; j<(Math.random() * 5|0); j++){
+                                    message += message;
+                                }
+                                logger.log('group' + i, message);
+                            }
+
+
+                            setTimeout(function showReservedErrors () {
+                                // --------------------------------------
+                                // Finally, show some reserved messages
+                                // --------------------------------------
+                                logger.log('warn', 'This is a warning!', {message: 'It may not work'});
+                                logger.log('error', 'This is an error!', {message: 'It did not work'});
+
+                            }, baseDelay);
+                        }, baseDelay);
+                    }, baseDelay);
+                }, baseDelay);
+            }, baseDelay);
+        }, baseDelay);
+    }, baseDelay);
+}, baseDelay);
